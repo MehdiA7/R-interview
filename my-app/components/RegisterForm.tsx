c869ec967f7ec80865d3184380bda043a66b387f
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-05-02 15:25:56 +0200

    put the logic on the button to select him

--- my-app/components/RegisterForm.tsx
+++ my-app/components/RegisterForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { FaStoreAlt, FaUserAstronaut } from "react-icons/fa";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm, SubmitHandler, Controller } from "react-hook-form"
@@ -14,23 +14,31 @@ const RegisterForm = () => {
         resolver: zodResolver(newUserSchema)
     });
 
+    const [typeButton, setTypeButton] = useState("");
+
     const createANewUser: SubmitHandler<NewUser> = (data) => {
         console.log(data);
     };
+
+    const selectButton = (payload: string) => {
+        setTypeButton(payload);
+    }
 
     return (
         <div>
             <div className="flex flex-row space-x-4">
                 <button
-                    className="flex flex-row items-center justify-center bg-black text-white w-40 h-10 
-                rounded-2xl border-1 border-gray-400 hover:bg-white hover:text-black transition-all duration-300"
+                    className={`flex flex-row items-center justify-center ${typeButton === "Merchant" ? "bg-black text-white" : "bg-white text-black"} w-40 h-10 
+                    rounded-2xl border-1 border-gray-400 hover:bg-black hover:text-white transition-all duration-300`}
+                onClick={(e) => selectButton("Merchant")}
                 >
                     <FaStoreAlt className="mr-2" />
                     Merchant
                 </button>
                 <button
-                    className="flex flex-row items-center justify-center bg-white text-black w-40 h-10 
-                rounded-2xl border-1 border-gray-400 hover:bg-black hover:text-white transition-all duration-300"
+                    className={`flex flex-row items-center justify-center ${typeButton === "Agent" ? "bg-black text-white" : "bg-white text-black"} w-40 h-10 
+                        rounded-2xl border-1 border-gray-400 hover:bg-black hover:text-white transition-all duration-300`}
+                    onClick={(e) => selectButton("Agent")}
                 >
                     <FaUserAstronaut className="mr-2" />
                     Agent
