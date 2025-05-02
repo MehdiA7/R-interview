8543f3193dc309d17781a2f0c8028440b23082ed
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-05-02 12:46:04 +0200

    First commit and add new register page with 2 buttons

---
+++ my-app/app/components/RegisterForm.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React from "react";
+import { FaStoreAlt, FaUserAstronaut } from "react-icons/fa";
+
+const RegisterForm = () => {
+    return (
+        <div>
+            <div className="flex flex-row space-x-4">
+                <button
+                    className="flex flex-row items-center justify-center bg-black text-white w-40 h-10 
+                rounded-2xl border-1 border-gray-400 hover:bg-white hover:text-black transition-all duration-300"
+                >
+                    <FaStoreAlt className="mr-2" />
+                    Merchant
+                </button>
+                <button
+                    className="flex flex-row items-center justify-center bg-white text-black w-40 h-10 
+                rounded-2xl border-1 border-gray-400 hover:bg-black hover:text-white transition-all duration-300"
+                >
+                    <FaUserAstronaut className="mr-2" />
+                    Agent
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default RegisterForm;
---
+++ my-app/app/register/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import RegisterForm from '../components/RegisterForm'
+
+const RegisterPage = () => {
+  return (
+    <div className='flex flex-col items-center justify-center mt-40'>
+        <h1 className='text-4xl font-medium mb-20'>Create an account</h1>
+        <RegisterForm/>
+    </div>
+  )
+}
+
+export default RegisterPage
