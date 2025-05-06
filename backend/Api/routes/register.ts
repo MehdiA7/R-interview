8d997646fd61563068ed31b4349c35f813085b5c
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-05-06 07:14:02 +0200

    Add back end verification for the register

--- backend/Api/routes/register.ts
+++ backend/Api/routes/register.ts
@@ -15,19 +15,105 @@ router.post("/", logger, async (req: Request, res: Response) => {
     const saltRounds = 10;
     let theBody: RegisterBody = req.body;
 
-    const verifyEmail: EmailExist[] = await _db.emailExist(theBody.email);
+    if (
+        !theBody.type ||
+        !theBody.firstname ||
+        !theBody.country ||
+        !theBody.email ||
+        !theBody.industry ||
+        !theBody.phone ||
+        !theBody.password ||
+        theBody.policy === undefined
+    ) {
+        res.status(400).send({
+            success: false,
+            message: "All field is required",
+        });
+        return;
+    }
 
+    const validTypes = ["Merchant", "Agent"];
+    if (!validTypes.includes(theBody.type)) {
+        res.status(400).send({
+            success: false,
+            message: "Type incorrect.",
+        });
+        return;
+    }
+
+    if (theBody.firstname.length < 3) {
+        res.status(400).send({
+            success: false,
+            message: "Firstname is incorrect, 3char min",
+        });
+        return;
+    }
+
+    const validCountries = ["Belgium", "France", "Other"];
+    if (!validCountries.includes(theBody.country)) {
+        res.status(400).send({
+            success: false,
+            message: "Country is incorrect",
+        });
+        return;
+    }
+
+    const validIndustry = ["Finance", "Medical", "Auto", "Energy"];
+    if (!validIndustry.includes(theBody.industry)) {
+        res.status(400).send({
+            success: false,
+            message: "Industry is incorrect",
+        });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(theBody.email)) {
+        res.status(400).send({
+            success: false,
+            message: "Email format is invalid",
+        });
+        return;
+    }
+
+    if (theBody.policy !== true) {
+        res.status(400).send({
+            success: false,
+            message: "Policy is incorect, You need to accept the policy",
+        });
+        return;
+    }
+
+    const verifyEmail: EmailExist[] = await _db.emailExist(theBody.email);
     if (verifyEmail.length > 0) {
-        res.status(409).send({ success: false, message: 'This email is taken !'});
-    return;
+        res.status(409).send({
+            success: false,
+            message: "This email is taken",
+        });
+        return;
+    }
+
+    if (
+        theBody.password.length < 8 ||
+        !/\d/.test(theBody.password) ||
+        !/[a-zA-Z]/.test(theBody.password)
+    ) {
+        res.status(400).send({
+            success: false,
+            message: "Password is incorrect, 8char min",
+        });
+        return;
     }
 
     const salt = await bcrypt.genSalt(saltRounds);
     const hashedPassword = await bcrypt.hash(theBody.password, salt);
     theBody.password = hashedPassword;
     _db.createUser(theBody);
-    
-    res.status(201).send({ success: true, message: `The user ${theBody.firstname} are created !` });
+
+    res.status(201).send({
+        success: true,
+        message: `The user ${theBody.firstname} are created !`,
+    });
+    return;
 });
 
 module.exports = router;
